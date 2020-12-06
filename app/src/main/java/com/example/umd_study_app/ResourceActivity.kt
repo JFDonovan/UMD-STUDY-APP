f9fee51da98b083c85d7844252e3a400fe5386e6
Edward Tong <etong@umd.edu>
2020-12-06 18:37:08 -0500

    EdTongBranch

--- app/src/main/java/com/example/umd_study_app/ResourceActivity.kt
+++ app/src/main/java/com/example/umd_study_app/ResourceActivity.kt
@@ -1,4 +1,140 @@
 package com.example.umd_study_app
 
-class ResourceActivity {
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import kotlinx.android.synthetic.main.activity_resource.*
+
+
+class ResourceActivity : AppCompatActivity(){
+
+    //these are the views
+    lateinit var ViewStatus : TextView
+    var editTextFilename: EditText? = null
+    var progressBar: ProgressBar? = null
+
+    //the firebase objects for storage and database
+    var mStorageReference: StorageReference? = null
+    var mDatabaseReference: DatabaseReference? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resource)
+
+        mStorageReference = FirebaseStorage.getInstance().reference
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference(DATABASE_PATH_UPLOADS)
+
+        ViewStatus = findViewById<TextView>(R.id.ViewStatus_text)
+        editTextFilename = findViewById<EditText>(R.id.editTextFileName)
+        progressBar = findViewById<ProgressBar>(R.id.progressbar)
+
+
+
+        val uploadButton = findViewById<Button>(R.id.uploadButton)
+        val viewUploads = findViewById<TextView>(R.id.ViewUploads_text)
+        //val viewResource = findViewById<Button>(R.id.viewResources)
+
+        uploadButton.setOnClickListener{
+           //TODO: Add functionality for the upload Button
+            getPDF()
+        }
+        viewUploads.setOnClickListener {
+            //GoToDangerousActivity::class.java
+            val intent = Intent(this, ViewFiles::class.java)
+            startActivity(intent)
+
+        }
+        // TODO: Add a listener for the viewUploads editText thing
+    }
+    //gets the PDF file that was uploaded
+    private fun getPDF(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+            return
+        }
+
+        val intent = Intent()
+        intent.type = "application/pdf"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PDF_CODE)
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PDF_CODE && resultCode == RESULT_OK && data != null && data.data != null) {
+            //if a file is selected
+            if (data.data != null) {
+                //uploading the file
+                uploadFile(data.data!!)
+            }else{
+                Toast.makeText(this, "No file chosen", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private fun uploadFile(data: Uri){
+        progressBar!!.visibility = View.VISIBLE
+        val sRef = mStorageReference!!.child(STORAGE_PATH_UPLOADS + System.currentTimeMillis() + ".pdf")
+        sRef.putFile(data).addOnSuccessListener { taskSnapshot ->
+            progressBar!!.visibility = View.GONE
+            ViewStatus.text = "File Uploaded Successfully"
+
+
+            val dlURL = taskSnapshot.storage.downloadUrl.toString()
+
+            val upload = Upload(
+                editTextFilename!!.text.toString(), dlURL
+            )
+            mDatabaseReference!!.child(mDatabaseReference!!.push().key!!).setValue(upload)
+
+        }.addOnFailureListener {
+            exception ->
+                Toast.makeText(getApplicationContext(), exception.message, Toast.LENGTH_LONG).show()
+
+        }.addOnProgressListener {
+            taskSnapshot->
+                val progress: Double =
+                    100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
+                val str: String = progress.toString().plus(" ").plus("% Uploading...")
+                ViewStatus.text = str
+
+        }
+
+
+    }
+
+    companion object{
+        val STORAGE_PATH_UPLOADS = "uploads/"
+        val DATABASE_PATH_UPLOADS = "uploads"
+        val PDF_CODE = 2342
+
+
+    }
+
 }
--- app/src/main/java/com/example/umd_study_app/ViewFiles.kt
+++ app/src/main/java/com/example/umd_study_app/ViewFiles.kt
@@ -1,4 +1,63 @@
 package com.example.umd_study_app
 
-class ViewFiles {
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+//import java.util.ArrayList
+//import java.util.List
+
+class ViewFiles : AppCompatActivity(){
+
+    var listView: ListView? = null
+
+    //database reference to get uploads data
+    var mDatabaseReference: DatabaseReference? = null
+
+    //list to store uploads data
+    lateinit var uploadList: List<Upload>
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_files)
+        uploadList = arrayListOf()
+        listView = findViewById<ListView>(R.id.listView) as ListView
+
+        listView!!.setOnItemClickListener{ parent, view, position, id ->
+            val upload = uploadList[position]
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(upload.url)
+            startActivity(intent)
+
+        }
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference(DATABASE_PATH_UPLOADS)
+        mDatabaseReference!!.addValueEventListener
+
+
+
+
+
+
+
+    }
+
+
+
+    companion object{
+        val STORAGE_PATH_UPLOADS = "uploads/"
+        val DATABASE_PATH_UPLOADS = "uploads"
+        val PDF_CODE = 2342
+
+    }
+
+
+
 }
