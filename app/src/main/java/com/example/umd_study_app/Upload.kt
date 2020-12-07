c07993eb3aed441cb995c6520dcfce2eaad88dc0
Edward Tong <etong@umd.edu>
2020-12-07 11:26:12 -0500

    EdTongBranch

--- app/src/main/java/com/example/umd_study_app/Upload.kt
+++ app/src/main/java/com/example/umd_study_app/Upload.kt
@@ -1,4 +1,10 @@
 package com.example.umd_study_app
 
-class Upload {
-}
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Upload (val name: String, val url : String)
+
+
+
+
